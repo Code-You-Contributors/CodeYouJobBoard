f827ae95db8a2e268bb567dcedd08943ef3b8306
Tyler <tyler.s9@proton.me>
2025-08-10 23:56:45 -0400

    Job Board implementation is complete. Need to add pagination and fix the search functionality next

--- src/app.js
+++ src/app.js
@@ -23,7 +23,9 @@ app.get('/api/sheet', async (req, res) => {
   console.log('Using API Key:', API_KEY ? 'Found' : 'Missing');
   console.log('Using Spreadsheet ID:', spreadsheetId ? 'Found' : 'Missing');
 
-  const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/JobBoard!A1:I196?majorDimension=COLUMNS&key=${API_KEY}`;
+  // Dynamic range for the Sheets API. Currently, A:I are the in-use columns
+  const range = encodeURIComponent(req.query.range || 'JobBoard!A:I');
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?majorDimension=COLUMNS&key=${API_KEY}`;
 
   try {
     const response = await axios.get(url);
@@ -32,7 +34,7 @@ app.get('/api/sheet', async (req, res) => {
     console.error('Google Sheets API error:', error.response ? error.response.data : error.message);
     res.status(500).json({ error: 'Error fetching Google Sheet' });
   }
-})
+});
 
 app.listen(PORT, () => {
   console.log('Server started at http://localhost:' + PORT);
